4fb305e69b0124f6cee42b2eebf7f2477ddd882e
Kevin van den Hoek <kevinvandenhoek@gmail.com>
2019-02-10 20:56:52 +0100

    Update tf_layout_widgets.dart
    exposed more classes

--- lib/tf_layout_widgets.dart
+++ lib/tf_layout_widgets.dart
@@ -1,3 +1,7 @@
 library tf_layout_widgets;
 
+export 'package:tf_layout_widgets/src/pages/pages.dart';
 export 'package:tf_layout_widgets/src/pages/navigation_stack_page/extensions/animated_navigation_stack_page.dart';
+export 'package:tf_layout_widgets/src/pages/navigation_stack_page/navigation_stack_page.dart';
+
+export 'package:tf_layout_widgets/src/pages/top_bar_page/top_bar_page.dart';
