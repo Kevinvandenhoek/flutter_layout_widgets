41e91b68198b7ee07b5fcfd562475be90736777a
Kevin van den Hoek <kevinvandenhoek@gmail.com>
2019-02-10 18:19:49 +0100

    added test class to try to import

--- lib/tf_layout_widgets.dart
+++ lib/tf_layout_widgets.dart
@@ -1,3 +1,9 @@
 library tf_layout_widgets;
 
+import 'package:tf_layout_widgets/widgets/pages/navigation_stack_page/extensions/animated_navigation_stack_page.dart';
+
 export 'package:tf_layout_widgets/widgets/pages/navigation_stack_page/extensions/animated_navigation_stack_page.dart';
+
+class Lelniac {
+  AnimatedNavigationStackPage page;
+}
